ebe935785b2ffd78300966ad36baaea463ac78ea
Matthias Endler <matthias-endler@gmx.net>
2017-11-26 12:09:54 +0100

    Refactoring

--- lama.py
+++ lama.py
@@ -1,7 +1,6 @@
 from cocos.sprite import Sprite
-from cocos.tiles import load
 from cocos.mapcolliders import RectMapCollider
-from cocos.layer import ScrollingManager, ScrollableLayer, ColorLayer
+from cocos.layer import ScrollingManager, ScrollableLayer
 from cocos.director import director
 from cocos.scene import Scene
 from cocos.actions import Action
@@ -12,6 +11,38 @@ WIDTH = 700
 HEIGHT = 500
 keyboard = key.KeyStateHandler()
 
+class Lama(ScrollableLayer):
+    def __init__(self):
+        super(Lama, self).__init__()
+
+        # And, just like last time, we make our sprite and have it do the action we define
+        self.sprite = Sprite(pyglet.image.load_animation("assets/img/lama_jump.gif"))
+        self.sprite.position = 150, 100
+        self.add(self.sprite)
+        self.sprite.do(GameAction())
+
+
+class Block(ScrollableLayer):
+    def __init__(self):
+        super(Block, self).__init__()
+
+        # And, just like last time, we make our sprite and have it do the action we define
+        self.sprite = Sprite("assets/img/block.png")
+        self.sprite.position = 150, 150
+        self.sprite.scale = 0.4
+        self.add(self.sprite)
+
+
+class Level(ScrollableLayer):
+    def __init__(self):
+        super(Level, self).__init__()
+
+        # And, just like last time, we make our sprite and have it do the action we define
+        self.sprite = Sprite(pyglet.image.load_animation("assets/img/level1.gif"))
+        self.sprite.position = 350, 250
+        self.sprite.scale = 2.3
+        self.add(self.sprite)
+
 
 class GameAction(Action, RectMapCollider):
     # Use the start function instead of  __init__
@@ -21,7 +52,7 @@ class GameAction(Action, RectMapCollider):
 
     def on_ground(self):
         rect = self.target.get_rect()
-        return rect.y <= -200
+        return rect.y <= 0
 
     def on_bump_handler(self, vx, vy):
         return vx, vy
@@ -64,44 +95,10 @@ class GameAction(Action, RectMapCollider):
         # scroller.set_focus(*new_rect.center)
 
 
-# Now, once again, we make another class for the sprite's layer
-# Remember that it must be a ScrollableLayer
-class Lama(ScrollableLayer):
-    def __init__(self):
-        super(Lama, self).__init__()
-
-        # And, just like last time, we make our sprite and have it do the action we define
-        self.sprite = Sprite(pyglet.image.load_animation("assets/img/lama_jump.gif"))
-        self.add(self.sprite)
-        self.sprite.do(GameAction())
-
-
-class Block(ScrollableLayer):
-    def __init__(self):
-        super(Block, self).__init__()
-
-        # And, just like last time, we make our sprite and have it do the action we define
-        self.sprite = Sprite("assets/img/block.png")
-        self.sprite.position = 150, 150
-        self.sprite.scale = 0.4
-        self.add(self.sprite)
-
-
-class Level(ScrollableLayer):
-    def __init__(self):
-        super(Level, self).__init__()
-
-        # And, just like last time, we make our sprite and have it do the action we define
-        self.sprite = Sprite(pyglet.image.load_animation("assets/img/level1.gif"))
-        self.sprite.position = 350, 250
-        self.sprite.scale = 2.3
-        self.add(self.sprite)
-
 
 def main():
     director.init(caption="LAMA", width=WIDTH, height=HEIGHT, autoscale=False, resizable=False)
 
-    scroller = ScrollingManager()
     director.window.push_handlers(keyboard)
 
     # The first thing we do in our "main" code is make the layer we just defined
@@ -125,11 +122,8 @@ def main():
     # First I add the map, and set the "z" to 0
     # The z is the vertical axis, so the highest z value layer will always show on top of the others
 
-    # Then I add the sprite, and set the z to 1 so that it shows on top of the map layer
-    scroller.add(player, z=1)
-
     scene = Scene()
-    scene.add(scroller, z=1)
+    scene.add(player, z=1)
 
     block = Block()
     scene.add(block, z=1)
