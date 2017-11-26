98f0bbfc86f897683ffc87a7a7cdf90721bf2d41
Matthias Endler <matthias-endler@gmx.net>
2017-11-26 15:19:58 +0100

    Let's go crazy

--- llama.py
+++ llama.py
@@ -10,10 +10,28 @@ import cocos.euclid as eu
 from collections import defaultdict
 from pyglet.window import key
 import pyglet
+from cocos.actions import *
+
+from cocos.layer import Layer
+from cocos.director import director
+
+from cocos.audio.pygame.mixer import Sound
+from cocos.audio.pygame import mixer
 
 WIDTH = 700
 HEIGHT = 500
 
+
+class Audio(Sound):
+    def __init__(self, audio_file):
+        super(Audio, self).__init__(audio_file)
+
+
+class AudioLayer(Layer):
+    def __init__(self):
+        super(AudioLayer, self).__init__()
+        song = Audio("assets/sound/LamaSong.ogg")
+        song.play(-1)
 
 class Background(Sprite):
     def __init__(self):
@@ -21,13 +39,15 @@ class Background(Sprite):
         self.position = 350, 250
         self.scale = 2.3
 
+    def drunk(self):
+        self.do(Reverse(Waves(duration=2000)))
+
 
 class Block(Sprite):
     def __init__(self, x, y, pic):
         super(Block, self).__init__(pic)
         self.position = pos = eu.Vector2(x, y)
         self.cshape = cm.CircleShape(pos, self.width / 2 - 10)
-
 
 class Lama(Sprite):
     def __init__(self, x, y):
@@ -39,7 +59,14 @@ class Lama(Sprite):
 
     def move(self, dx, dy):
         x, y = self.position
-        self.position = (x + self.speed * dx, y + self.strength * dy)
+
+        new_x, new_y = (x + self.speed * dx, y + self.strength * dy)
+
+        if new_x < 0:
+            new_x = 0
+        if new_x > WIDTH:
+            new_x = WIDTH
+        self.position = (new_x, new_y)
         self.cshape.center = self.position
 
     def powerup(self):
@@ -57,10 +84,10 @@ class Game(ColorLayer):
     def __init__(self):
         super(Game, self).__init__(0, 80, 125, 0)
 
-        self.block_pic = "assets/img/block.png"
+        self.block_pic = "assets/img/heart.png"
         for pos in [(100, 100), (540, 380), (540, 100), (100, 380)]:
             self.add(Block(pos[0], pos[1], self.block_pic))
-        self.lama = Lama(120, 100)
+        self.lama = Lama(WIDTH/2, 100)
         self.add(self.lama)
 
         cell = self.lama.width * 1.25
@@ -68,6 +95,9 @@ class Game(ColorLayer):
 
         self.pressed = defaultdict(int)
         self.schedule(self.update)
+
+        #self.kiss_sound = Sound("assets/sound/kiss.ogg")
+        self.jump_sound = Sound("assets/sound/jump.ogg")
 
     def on_key_press(self, k, m):
         self.pressed[k] = 1
@@ -82,11 +112,16 @@ class Game(ColorLayer):
         for other in self.collman.iter_colliding(self.lama):
             self.remove(other)
             self.lama.powerup()
+            #self.kiss_sound.play()
+
+        if len(self.children) <= 1:
+            pass
 
         dx = (self.pressed[key.RIGHT] - self.pressed[key.LEFT]) * 250 * dt
         dy = 0
         if self.lama.on_ground():
             if self.pressed[key.SPACE]:
+                self.jump_sound.play()
                 dy = 150
         else:
             dy -= 300 * dt
@@ -94,8 +129,11 @@ class Game(ColorLayer):
 
 
 def main():
-    director.init(caption="Lama!!!", width=WIDTH, height=HEIGHT)
+    mixer.init()
+
+    director.init(caption="Llama!!!", width=WIDTH, height=HEIGHT)
     scene = Scene()
+    scene.add(AudioLayer())
     scene.add(Background(), z=0)
     scene.add(Game(), z=1)
     director.run(scene)
